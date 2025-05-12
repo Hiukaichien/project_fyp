34509334e7feba4fdd8d10705c68173d23e782ed
Lee <xinhao1016@gmail.com>
2025-05-12 10:45:43 +0800

    improve readability for route

--- routes/web.php
+++ routes/web.php
@@ -23,11 +23,37 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::get('/kertas_siasatan/upload', [KertasSiasatanController::class, 'create'])->name('kertas_siasatan.upload');
-    Route::get('/kertas_siasatan/project', [ProjectController::class, 'index'])->name('kertas_siasatan.project');
+
+    Route::get('/kertas_siasatan/upload', [KertasSiasatanController::class, 'create'])->name('kertas_siasatan.create');
     
-    // Add the resource routes for Kertas Siasatan here
-    Route::resource('kertas_siasatan', KertasSiasatanController::class);
+    // Explicit routes for KertasSiasatanController
+    Route::get('/kertas_siasatan', [KertasSiasatanController::class, 'index'])->name('kertas_siasatan.index');
+    Route::post('/kertas_siasatan', [KertasSiasatanController::class, 'store'])->name('kertas_siasatan.store');
+    Route::get('/kertas_siasatan/{kertas_siasatan}', [KertasSiasatanController::class, 'show'])->name('kertas_siasatan.show');
+    Route::get('/kertas_siasatan/{kertas_siasatan}/edit', [KertasSiasatanController::class, 'edit'])->name('kertas_siasatan.edit');
+    Route::put('/kertas_siasatan/{kertas_siasatan}', [KertasSiasatanController::class, 'update'])->name('kertas_siasatan.update');
+    Route::patch('/kertas_siasatan/{kertas_siasatan}', [KertasSiasatanController::class, 'update']); // Also common for updates
+    Route::delete('/kertas_siasatan/{kertas_siasatan}', [KertasSiasatanController::class, 'destroy'])->name('kertas_siasatan.destroy');
+    
+    // New route for exporting Kertas Siasatan by project
+    Route::get('/kertas_siasatan/export-by-project', [KertasSiasatanController::class, 'exportByProject'])->name('kertas_siasatan.export_by_project');
+
+    // Explicit routes for ProjectController
+    Route::get('/projects', [ProjectController::class, 'index'])->name('projects.index');
+    Route::get('/projects/create', [ProjectController::class, 'create'])->name('projects.create');
+    Route::post('/projects', [ProjectController::class, 'store'])->name('projects.store');
+    Route::get('/projects/{project}', [ProjectController::class, 'show'])->name('projects.show');
+    Route::get('/projects/{project}/edit', [ProjectController::class, 'edit'])->name('projects.edit');
+    Route::put('/projects/{project}', [ProjectController::class, 'update'])->name('projects.update'); // Handles PUT requests
+    Route::patch('/projects/{project}', [ProjectController::class, 'update']); // Also common to handle PATCH for updates
+    Route::delete('/projects/{project}', [ProjectController::class, 'destroy'])->name('projects.destroy');
+
+    // New route for associating a paper with a project
+    Route::post('/projects/{project}/associate-paper', [ProjectController::class, 'associatePaper'])->name('projects.associate_paper');
+    
+    // Route for disassociating a paper from a project
+    Route::post('/projects/{project}/disassociate-paper/{paperType}/{paperId}', [ProjectController::class, 'disassociatePaper'])->name('projects.disassociate_paper');
+
     // The route named 'kertas_siasatan.upload' used in the index view
     // corresponds to the 'create' method handled by Route::resource.
 });
