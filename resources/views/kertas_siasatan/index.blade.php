c01985145517dbc33545cae03ef6ea896b3e7bff
Lee <xinhao1016@gmail.com>
2025-05-12 14:21:15 +0800

    modify button name

--- resources/views/kertas_siasatan/index.blade.php
+++ resources/views/kertas_siasatan/index.blade.php
@@ -72,7 +72,7 @@
                 {{-- Upload Button --}}
                     <a href="{{ route('kertas_siasatan.create') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded inline-flex items-center">
                         <svg class="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
-                        Muat Naik Excel
+                        Import/Export
                     </a>
                 </div>
 
