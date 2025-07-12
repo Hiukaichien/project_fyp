e1d36ec56547db2c6c216e3a4087a94e60ae58ea
Lee <xinhao1016@gmail.com>
2025-07-12 20:18:45 +0800

    add forget password

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -32,12 +32,18 @@
             </label>
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Lupa kata laluan?') }}
+        <div class="flex items-center justify-between mt-4">
+            <div>
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
+                    {{ __('Tiada akaun? Daftar') }}
                 </a>
-            @endif
+                
+                @if (Route::has('password.request'))
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ms-4" href="{{ route('password.request') }}">
+                        {{ __('Lupa kata laluan?') }}
+                    </a>
+                @endif
+            </div>
 
             <x-primary-button class="ms-3">
                 {{ __('Log Masuk') }}
